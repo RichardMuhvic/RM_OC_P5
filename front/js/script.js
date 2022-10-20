c995b8fd03bde87e7d5f9853edb97e8f612fbdbf
Richard <richardmuhvic@gmail.com>
2022-10-20 16:14:19 +0200

    je déclare la variable products

--- front/js/script.js
+++ front/js/script.js
@@ -12,49 +12,50 @@ fetch("http://localhost:3000/api/products")
         console.log(err);
     });
 
-// Déclaration de la constante products qui sera un produit obtenu de l'API :
+// Déclaration de la variable products :
 let products;
 
-// Création de la boucle 'for' pour afficher les produits :
-for(let i=0; i<products[i].length; i++ ) {
-    console.log(products);
-    // Création de la section article qui va recevoir tous les élements des produits :
-    const sectionArticle = document.querySelector("#items");
+function items(products) {
+    // Création de la boucle 'for' pour afficher les produits :
+    for(let i=0; i<products[i].length; i++ ) {
+        // Création de la section article qui va recevoir tous les élements des produits :
+        const sectionArticle = document.querySelector(".items");
 
-    // Création des éléments dans le DOM :
-    const products = products[i];
+        // Création des éléments dans le DOM :
+        const productId = document.createElement("a");
+        productId = products._id;
 
-    const productId = document.createElement("a");
-    productId = products._id;
+        const productImage = document.createElement("img");
+        productImage = products.imageUrl;
 
-    const productImage = document.createElement("img");
-    productImage = products.imageUrl;
+        const productName = document.createElement("h3");
+        productName = products.name;
 
-    const productName = document.createElement("h3");
-    productName = products.name;
+        const productDescription = document.createElement("p");
+        productDescription = products.description;
 
-    const productDescription = document.createElement("p");
-    productDescription = products.description;
+        const productAltTxt = document.createElement("alt");
+        productAltTxt = products.altTxt;
 
-    const productAltTxt = document.createElement("alt");
-    productAltTxt = products.altTxt;
+        const productPrice = document.createElement("p");
+        productPrice = products.price;
 
-    const productPrice = document.createElement("p");
-    productPrice = products.price;
+        // Parenter les éléments à la section article :
+        sectionArticle.appendChild(productId);
+        sectionArticle.appendChild(productImage);
+        sectionArticle.appendChild(productName);
+        sectionArticle.appendChild(productDescription);
 
-    // Parenter les éléments à la section article :
-    sectionArticle.appendChild(productId);
-    sectionArticle.appendChild(productImage);
-    sectionArticle.appendChild(productName);
-    sectionArticle.appendChild(productDescription);
-
-    // Création du block html du DOM :
-    sectionArticle.querySelector("#items").innerHTML += 
-    `<a href="./product.html?id=${productId}">
-        <article>
-            <img src=${productImage} alt=${productAltTxt}>
-            <h3 class="productName">${productName}</h3>
-            <p class="productDescription">${productDescription}</p>
-        </article>
-    </a>`;
+        // Création du block html du DOM :
+        sectionArticle.querySelector("#items").innerHTML += 
+        `<a href="./product.html?id=${productId}">
+            <article>
+                <img src=${productImage} alt=${productAltTxt}>
+                <h3 class="productName">${productName}</h3>
+                <p class="productDescription">${productDescription}</p>
+            </article>
+        </a>`;
+    };
 };
+
+items(products);
