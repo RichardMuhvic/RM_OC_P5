208ea72761cdc20a55973df9eb1db55106e99bc3
Richard <richardmuhvic@gmail.com>
2022-10-22 01:12:34 +0200

    retirer la fonction et ajouter [i] dans el bloc kHTML de la boucle for

--- front/js/script.js
+++ front/js/script.js
@@ -15,49 +15,43 @@ fetch("http://localhost:3000/api/products")
 // Déclaration de la variable products :
 let products;
 
-function items(products) {
-    // Création de la boucle 'for' pour afficher les produits :
-    for( let i=0; i<products.length; i++ ) {
+for( let i = 0; i < products.length; i++ ) {
+    // Création de la section article qui va recevoir tous les élements des produits :
+    const sectionBlock = document.querySelector(".items");
 
-        // Création de la section article qui va recevoir tous les élements des produits :
-        const sectionBlock = document.querySelector(".items");
+    // Création d'une balise article pour accueillir l'ensemble du block :
+    const sectionArticle = document.createElement("article");
 
-        // Création d'une balise article pour accueillir l'ensemble du block :
-        const sectionArticle = document.createElement("article");
+    // Création des éléments dans le DOM :
+    const productId = document.createElement("a");
+    // On accède à l'indice 'i' de la liste product pour configurer la source de l'ID :
+    productId = products[i]._id;
+    // On rattache l'ID à sectionArticle (la balise article) :
+    sectionArticle.appendChild(productId);
 
-        // Création des éléments dans le DOM :
-        const productId = document.createElement("a");
-        // On accède à l'indice 'i' de la liste product pour configurer la source de l'ID :
-        productId = products[i]._id;
-        // On rattache l'ID à sectionArticle (la balise article) :
-        sectionArticle.appendChild(productId);
+    // On recommence les trois opérations précédentes pour image, name, description:
+    const productImage = document.createElement("img");
+    productImage = products[i].imageUrl;
+    sectionArticle.appendChild(productImage);
 
-        // On recommence les trois opérations précédentes pour image, name, description:
-        const productImage = document.createElement("img");
-        productImage = products[i].imageUrl;
-        sectionArticle.appendChild(productImage);
+    const productName = document.createElement("h3");
+    productName = products[i].name;
+    sectionArticle.appendChild(productName);
 
-        const productName = document.createElement("h3");
-        productName = products[i].name;
-        sectionArticle.appendChild(productName);
+    const productDescription = document.createElement("p");
+    productDescription = products[i].description;
+    sectionArticle.appendChild(productDescription);
 
-        const productDescription = document.createElement("p");
-        productDescription = products[i].description;
-        sectionArticle.appendChild(productDescription);
+    // On rattache la balise article à la section fiches
+	sectionBlock.appendChild(sectionArticle);
 
-        // On rattache la balise article à la section fiches
-		sectionBlock.appendChild(sectionArticle);
-
-        // Création du block html du DOM :
-        sectionArticle.querySelector("#items").innerHTML += 
-        `<a href="./product.html?id=${productId}">
-            <article>
-                <img src=${productImage} alt=${productAltTxt}>
-                <h3 class="productName">${productName}</h3>
-                <p class="productDescription">${productDescription}</p>
-            </article>
-        </a>`;
-    };
+    // Création du block HTML du DOM :
+    sectionArticle.querySelector("#items").innerHTML += 
+    `<a href="./product.html?id=${products[i]._id}">
+        <article>
+            <img src=${products[i].imageUrlmage} alt=${products[i].altTxt}>
+            <h3 class="productName">${products[i].name}</h3>
+            <p class="productDescription">${products[i].description}</p>
+       </article>
+  </a>`;
 };
-
-items(products);
